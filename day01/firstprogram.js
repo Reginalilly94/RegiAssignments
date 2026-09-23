696c716544e64ea5a24678d11f809aacbbddcc08
Reginalilly94 <reginalillybsc13@gmail.com>
2026-09-23 13:24:06 +0530

    Add files via upload
    day01

---
+++ day01/firstprogram.js
@@ -0,0 +1,23 @@
+var word ="Hello,World!"
+var num = 786
+var boo = true
+var value // undefined
+var value1 = null
+console.log(word)
+console.log(num)
+console.log(boo)
+console.log(typeof value) //undefined
+console.log(typeof value1) //object
+
+let firstname = 'Regi'
+let companyname = "Testleaf"
+let mobilenumber = 9998756431
+let isautomation = true
+let hasplaywright //unassigned
+console.log(firstname)
+console.log(companyname)
+console.log(mobilenumber)
+console.log(isautomation)
+console.log(typeof hasplaywright) //unassigned
+
+
---
+++ day01/secondprogram.js
@@ -0,0 +1,16 @@
+const browser="chrome"
+function getBrowserVersion()
+{    // function scope
+    if(browser==="chrome")
+       // var browserversion="chrome158"
+    {     //block scope
+let browserversion="chrome158"
+         console.log(browser)
+                
+ console.log(browserversion)
+ 
+    }
+}
+   
+getBrowserVersion()
+
