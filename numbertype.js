cb23d6ccb2f734f8b110317916b7faf2c7e7125c
Reginalilly94 <reginalillybsc13@gmail.com>
2026-09-24 22:28:07 +0530

    Add files via upload
    day02

---
+++ numbertype.js
@@ -0,0 +1,17 @@
+let n=2
+function getnumber(n)
+{
+    if(n>0)
+    {
+        console.log("It is a Positive number")
+
+    }
+    else if(n<0)
+    {
+      console.log("It is a Negative number")
+  
+    }
+    else{
+        console.log("The Number is Neutral")
+    }
+}getnumber(n)
